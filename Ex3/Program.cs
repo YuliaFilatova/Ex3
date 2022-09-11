23d9d89f9be1952f17282eaf5163c4d641fa7625
Yuliya <filatovajulia@rambler.ru>
2022-09-11 15:03:57 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int[] GetCubNumber(int number)
+{
+    int[] cubNumber = new int[number];
+
+//заполнение массива результата
+for (int i = 1; i <= number; i++)
+{
+    cubNumber[i-1] = i * i * i;
+}
+    return cubNumber;
+
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int[] result = GetCubNumber(number);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
+
